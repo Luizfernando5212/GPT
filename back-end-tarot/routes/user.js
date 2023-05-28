037d2f6c43912a4f932c17f85bb313f1cd943a6f
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-05-28 03:32:03 -0300

    Update user.js

--- back-end-tarot/routes/user.js
+++ back-end-tarot/routes/user.js
@@ -12,6 +12,6 @@ router.post('/login', userController.verifyUser);
 
 router.post('/', userController.newUser);
 
-router.get('/', auth.authenticateToken, userController.getUsers);
+router.get('/', userController.getUsers);
 
 module.exports = router;
