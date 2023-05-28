7dc7b4a7c9b2495e9305bcfbd8aec9a8c398e7b3
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-05-28 01:51:33 -0300

    Update urls.js

--- back-end-tarot/util/urls.js
+++ back-end-tarot/util/urls.js
@@ -1,7 +1,5 @@
 exports.url = (numberId, token) => {
 
     return "https://graph.facebook.com/v12.0/" +
-    numberId +
-    "/messages?access_token=" +
-    token;
+    numberId + "/messages?access_token=";
 }
