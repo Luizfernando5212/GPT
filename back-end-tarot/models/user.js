b05702e5f8704e0a1c6b44d1da6bdce83fde0c13
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-05-28 03:51:54 -0300

    Update user.js

--- back-end-tarot/models/user.js
+++ back-end-tarot/models/user.js
@@ -21,34 +21,34 @@ var UserSchema = new Schema(
     }
 );
 
-UserSchema.pre('save', function (next) {
-    var user = this;
-    if (user.password) {
-        if (!user.isModified('password'))
-            return next();
+// UserSchema.pre('save', function (next) {
+//     var user = this;
+//     if (user.password) {
+//         if (!user.isModified('password'))
+//             return next();
 
-        bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-            if (err) return next(err);
+//         bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//             if (err) return next(err);
 
-            bcrypt.hash(user.password, salt, (err, hash) => {
-                if (err) return next(err);
+//             bcrypt.hash(user.password, salt, (err, hash) => {
+//                 if (err) return next(err);
 
-                user.password = hash;
-                next();
-            });
-        });
-    }
-});
+//                 user.password = hash;
+//                 next();
+//             });
+//         });
+//     }
+// });
 
-UserSchema.methods.comparePassword = async function (password) {
-    try {
-        const isMatch = await bcrypt.compare(password, this.password);
-        return isMatch;
-    } catch (err) {
-        console.log(err);
-    }
-    return false;
-}
+// UserSchema.methods.comparePassword = async function (password) {
+//     try {
+//         const isMatch = await bcrypt.compare(password, this.password);
+//         return isMatch;
+//     } catch (err) {
+//         console.log(err);
+//     }
+//     return false;
+// }
 
 
 
