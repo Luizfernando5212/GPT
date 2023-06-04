56bcd8fc0c65da377d653080795c8a5c0cf5477d
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-06-04 01:12:21 -0300

    Update userService.js

--- back-end-tarot/service/userService.js
+++ back-end-tarot/service/userService.js
@@ -150,3 +150,12 @@ exports.updateQuestion = async (req, res) => {
         console.log(err);
     }
 }
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const response = await User.findByIdAndDelete(req.params.id);
+        res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+}
