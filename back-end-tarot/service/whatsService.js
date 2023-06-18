41dc65c3c4fb4841b5fa57b03c5ecd9ae8bea18a
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-06-18 03:35:15 -0300

    Update whatsService.js

--- back-end-tarot/service/whatsService.js
+++ back-end-tarot/service/whatsService.js
@@ -94,8 +94,8 @@ exports.webHook = async (req, res) => {
                             await axios(request.textMessage(from,
                                 'Primeiro vamos estabelecer uma conexão energética. Me fale o seu *nome*.'));
 
-                            await axios(request.updateState(from, 1));
-                            await fetch()
+                            // await axios(request.updateState(from, 1));
+                            await fetch(url.updateState, request.updateStateFetch(from, 1))
 
                             // await axios(request.interactiveMessage(from, {
                             //     header: `Olá, seja bem vindo ${nome}`,
