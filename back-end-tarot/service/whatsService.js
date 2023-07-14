4d6dabc3141cae16f662a858596ebf5aa46c5989
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-07-14 20:25:21 -0300

    Update whatsService.js

--- back-end-tarot/service/whatsService.js
+++ back-end-tarot/service/whatsService.js
@@ -157,8 +157,9 @@ exports.webHook = async (req, res) => {
             console.log(body.entry[0].changes[0].value.messages[0].timestamp);
             console.log(Math.round(Date.now() / 1000))
             let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-
+            console.log('antes')
             await axios(request.mediaMessage(from, 'https://tarotai.onrender.com/naruto/sasuke'));
+            console.log('depois')
             // console.log(cheguei)
 
             // console.log(body.entry[0].changes[0].value.messages[0].timestamp);
