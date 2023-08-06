7617a3843ff7525192ce750d3b61f42f3d616bb8
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-08-06 19:13:57 -0300

    Update on mongoose package

--- back-end-tarot/test.js
+++ back-end-tarot/test.js
@@ -1,66 +1,102 @@
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-var imgSchema = require('./model.js');
-var fs = require('fs');
-var path = require('path');
-app.set("view engine", "ejs");
-require('dotenv').config();
+// var bodyParser = require('body-parser');
+// var mongoose = require('mongoose')
+// var imgSchema = require('./model.js');
+// var fs = require('fs');
+// var path = require('path');
+// app.set("view engine", "ejs");
+// require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(console.log("DB Connected"))
+// mongoose.connect(process.env.MONGO_URL)
+// .then(console.log("DB Connected"))
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
-// var multer = require('multer');
+// // var multer = require('multer');
 
-// var storage = multer.diskStorage({
-// 	destination: (req, file, cb) => {
-// 		cb(null, 'uploads')
-// 	},
-// 	filename: (req, file, cb) => {
-// 		cb(null, file.fieldname + '-' + Date.now())
+// // var storage = multer.diskStorage({
+// // 	destination: (req, file, cb) => {
+// // 		cb(null, 'uploads')
+// // 	},
+// // 	filename: (req, file, cb) => {
+// // 		cb(null, file.fieldname + '-' + Date.now())
+// // 	}
+// // });
+
+// // var upload = multer({ storage: storage });
+
+// // app.get('/', (req, res) => {
+// // 	imgSchema.find({})
+// // 	.then((data, err)=>{
+// // 		if(err){
+// // 			console.log(err);
+// // 		}
+// // 		res.render('imagepage',{items: data})
+// // 	})
+// // });
+
+
+// app.post('/', upload.single('image'), (req, res, next) => {
+
+// 	var obj = {
+// 		name: req.body.name,
+// 		desc: req.body.desc,
+// 		img: {
+// 			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+// 			contentType: 'image/png'
+// 		}
 // 	}
-// });
-
-// var upload = multer({ storage: storage });
-
-// app.get('/', (req, res) => {
-// 	imgSchema.find({})
-// 	.then((data, err)=>{
-// 		if(err){
+// 	imgSchema.create(obj)
+// 	.then ((err, item) => {
+// 		if (err) {
 // 			console.log(err);
 // 		}
-// 		res.render('imagepage',{items: data})
-// 	})
+// 		else {
+// 			// item.save();
+// 			res.redirect('/');
+// 		}
+// 	});
 // });
 
+// var port = process.env.PORT || '3000'
+// app.listen(port, err => {
+// 	if (err)
+// 		throw err
+// 	console.log('Server listening on port', port)
+// })
 
-app.post('/', upload.single('image'), (req, res, next) => {
 
-	var obj = {
-		name: req.body.name,
-		desc: req.body.desc,
-		img: {
-			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-			contentType: 'image/png'
-		}
+// let array = [1, 2, 3, 4, 4, 5, 6, 8, 8];
+
+// let result = array.reduce((acc, curr) =>  {acc += curr % 2 === 0 ? curr : 0}, 0);
+
+
+
+// console.log(result)
+let array2 = [9, 8, 7, 1, 2, 3, 4, 2, 8];
+const sortArray = (array) => {
+	let result = [];
+	let size = array.length;
+	for (let i = 0; i < size; i++) {
+
+		let remove = array.reduce((acc, curr) => {return acc > curr ? acc : curr}, 0)
+
+		result.unshift(remove);
+		let index = array.indexOf(remove);
+		array.splice(index, 1);
+
 	}
-	imgSchema.create(obj)
-	.then ((err, item) => {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			// item.save();
-			res.redirect('/');
-		}
-	});
-});
+	return result;
+}
 
-var port = process.env.PORT || '3000'
-app.listen(port, err => {
-	if (err)
-		throw err
-	console.log('Server listening on port', port)
-})
+console.log(sortArray(array2));
+// let test = []
+
+// test.push(1)
+// console.log(test)
+
+// test.push(2)
+// console.log(test)
+
+// test.shift()
+// console.log(test)
